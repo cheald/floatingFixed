af22f1eac5c9a860ab1b49159d3660e5eb53fabf
Chris Heald <cheald@gmail.com>
2011-01-20 20:52:39 -0700

    Initial commit

---
+++ jquery.floatingFixed.js
@@ -0,0 +1,73 @@
+/***********************************************************************
+This is a simple plugin to allow you to toggle an element between 
+position: absolute and position: fixed based on the window scroll 
+position. This lets you have an apparently inline element which floats 
+to stay on the screen once it would otherwise scroll off the screen. 
+
+Author: Chris Heald (cheald@mashable.com) 
+
+Copyright (c) 2011, Chris Heald All rights reserved. 
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are 
+met: 
+
+Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer. Redistributions in 
+binary form must reproduce the above copyright notice, this list of 
+conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution. Neither the name of the 
+Mashable nor the names of its contributors may be used to endorse or 
+promote products derived from this software without specific prior 
+written permission. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS 
+AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+***********************************************************************/
+
+(function($) {
+  var triggers = [];
+  $.fn.floatingFixed = function(options) {
+    options = $.extend({}, $.floatingFixed.defaults, options);
+    var r = $(this).each(function() {
+      var $this = $(this), pos = $this.position();
+      $this.data("floatingFixedTop", pos.top);
+      $this.data("floatingFixedOptions", options);
+      triggers.push($this);
+    });
+    windowScroll();    
+    return r;
+  }
+  
+  $.floatingFixed = $.fn.floatingFixed;
+  $.floatingFixed.defaults = {
+    padding: 0
+  }
+  
+  var $window = $(window);
+  var windowScroll = function() {
+    if(triggers.length === 0) { return; }
+    var scrollY = $window.scrollTop();
+    for(var i = 0; i < triggers.length; i++) {
+      var t = triggers[i];
+      var opt = t.data("floatingFixedOptions");
+      if(t.data("floatingFixedTop") < scrollY + opt.padding && !t.data("isFloating")) {
+        t.data("floatingFixedTopOrigPosition", t.css("position"));
+        t.data("floatingFixedTopOrigTop", t.css("top"));
+        t.css({position: 'fixed', top: opt.padding}).data("isFloating", true);
+      } else if(t.data("floatingFixedTop") >= scrollY + opt.padding && t.data("isFloating")) {
+        t.css({position: t.data("floatingFixedTopOrigPosition"), top: t.data("floatingFixedTopOrigTop")}).data("isFloating", false);
+      }
+    }
+  };
+  
+  $window.scroll(windowScroll);  
+})(jQuery);
